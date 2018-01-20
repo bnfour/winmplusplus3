27d0fd506858c7c14a650200005b0b74cbf4c9de
Bn4 <bananbl4@gmail.com>
2018-01-20 13:11:16 +0700

    MainForm tray icon and stubs for event handler implemented

--- ui/MainForm.Designer.cs
+++ ui/MainForm.Designer.cs
@@ -8,6 +8,13 @@ namespace winmplusplus3
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NotifyIcon trayIcon;
+		private System.Windows.Forms.ContextMenuStrip trayMenu;
+		private System.Windows.Forms.ToolStripMenuItem enabledToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem runAtStartupToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutWinMToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -30,8 +37,17 @@ namespace winmplusplus3
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.label1 = new System.Windows.Forms.Label();
+			this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.enabledToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.runAtStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.aboutWinMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.trayMenu.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -41,6 +57,61 @@ namespace winmplusplus3
 			this.label1.Size = new System.Drawing.Size(121, 23);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Third time\'s the charm?";
+			// 
+			// trayIcon
+			// 
+			this.trayIcon.ContextMenuStrip = this.trayMenu;
+			this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+			this.trayIcon.Text = "Win-M++";
+			this.trayIcon.Visible = true;
+			// 
+			// trayMenu
+			// 
+			this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.enabledToolStripMenuItem,
+			this.runAtStartupToolStripMenuItem,
+			this.toolStripSeparator1,
+			this.aboutWinMToolStripMenuItem,
+			this.quitToolStripMenuItem});
+			this.trayMenu.Name = "trayMenu";
+			this.trayMenu.Size = new System.Drawing.Size(164, 98);
+			// 
+			// enabledToolStripMenuItem
+			// 
+			this.enabledToolStripMenuItem.Checked = true;
+			this.enabledToolStripMenuItem.CheckOnClick = true;
+			this.enabledToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.enabledToolStripMenuItem.Name = "enabledToolStripMenuItem";
+			this.enabledToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+			this.enabledToolStripMenuItem.Text = "Enabled";
+			this.enabledToolStripMenuItem.Click += new System.EventHandler(this.EnabledToolStripMenuItemClick);
+			// 
+			// runAtStartupToolStripMenuItem
+			// 
+			this.runAtStartupToolStripMenuItem.CheckOnClick = true;
+			this.runAtStartupToolStripMenuItem.Name = "runAtStartupToolStripMenuItem";
+			this.runAtStartupToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+			this.runAtStartupToolStripMenuItem.Text = "Run at startup";
+			this.runAtStartupToolStripMenuItem.Click += new System.EventHandler(this.RunAtStartupToolStripMenuItemClick);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(160, 6);
+			// 
+			// aboutWinMToolStripMenuItem
+			// 
+			this.aboutWinMToolStripMenuItem.Name = "aboutWinMToolStripMenuItem";
+			this.aboutWinMToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+			this.aboutWinMToolStripMenuItem.Text = "About Win-M++";
+			this.aboutWinMToolStripMenuItem.Click += new System.EventHandler(this.AboutWinMToolStripMenuItemClick);
+			// 
+			// quitToolStripMenuItem
+			// 
+			this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+			this.quitToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+			this.quitToolStripMenuItem.Text = "Quit";
+			this.quitToolStripMenuItem.Click += new System.EventHandler(this.QuitToolStripMenuItemClick);
 			// 
 			// MainForm
 			// 
@@ -56,6 +127,8 @@ namespace winmplusplus3
 			this.ShowInTaskbar = false;
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "Win-M++ v. 3";
+			this.Shown += new System.EventHandler(this.MainFormShown);
+			this.trayMenu.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
--- ui/MainForm.cs
+++ ui/MainForm.cs
@@ -6,20 +6,80 @@ using System.Windows.Forms;
 namespace winmplusplus3
 {
 	/// <summary>
-	/// Description of MainForm.
+	/// Main form that houses tray icon and its menu.
 	/// </summary>
 	public partial class MainForm : Form
 	{
+		// TODO: add AboutForm field and references to AutorunManager and KeyBoardHookHandler.
+		
+		/// <summary>
+		/// MainForm constructor.
+		/// </summary>
 		public MainForm()
 		{
 			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
+			// The InitializeComponent() call is required 
+			// for Windows Forms designer support.
 			//
 			InitializeComponent();
 			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+			// TODO: pass the references for classes used throughout handlers
+			// TODO: set Run at startup value from AutorunManager
+		}
+		
+		/// <summary>
+		/// Form Shown event handler.
+		/// Hides the form.
+		/// </summary>
+		/// <param name="sender">Event sender, MainForm itself.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void MainFormShown(object sender, EventArgs e)
+		{
+			Hide();
+		}
+		
+		/// <summary>
+		/// Handler for "Enabled" item in the tray menu.
+		/// Enables or disables app action.
+		/// </summary>
+		/// <param name="sender">Event sender, enabledToolStripMenuItem.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void EnabledToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			// TODO: set KeyboardHookHandler state accordingly and update icon
+		}
+		
+		/// <summary>
+		/// Handler for "Run at startup" item in the tray menu.
+		/// Updates autorun state via AutorunManager.
+		/// </summary>
+		/// <param name="sender">Event sender, runAtStartupToolStripMenuItem.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void RunAtStartupToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			// TODO: set AutorunManager state accordingly
+		}
+		
+		/// <summary>
+		/// Handler for "About Win-M++" item in the tray menu.
+		/// Displays AboutForm instance from this class if not already shown.
+		/// </summary>
+		/// <param name="sender">Event sender, aboutWinMToolStripMenuItem.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void AboutWinMToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			// TODO: show AboutForm after it's designed
+		}
+		
+		/// <summary>
+		/// Handler for "Quit" item in the tray menu.
+		/// Obviously quits the program.
+		/// </summary>
+		/// <param name="sender">Event sender, quitToolStripMenuItem.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void QuitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			Application.Exit();
 		}
 	}
 }
