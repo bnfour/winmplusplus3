45c6483409dd2cc811873c04628b197fa3c86ac5
Bn4 <bananbl4@gmail.com>
2018-01-20 13:42:02 +0700

    AboutForm implemented

---
+++ ui/AboutForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace winmplusplus3
+{
+	partial class AboutForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.Label titleLabel;
+		private System.Windows.Forms.Label versionLabel;
+		private System.Windows.Forms.LinkLabel linkLabel;
+		private System.Windows.Forms.Label labelInfo;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.versionLabel = new System.Windows.Forms.Label();
+			this.linkLabel = new System.Windows.Forms.LinkLabel();
+			this.labelInfo = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+			this.pictureBox.InitialImage = null;
+			this.pictureBox.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(32, 32);
+			this.pictureBox.TabIndex = 0;
+			this.pictureBox.TabStop = false;
+			// 
+			// titleLabel
+			// 
+			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.titleLabel.Location = new System.Drawing.Point(50, 12);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(168, 32);
+			this.titleLabel.TabIndex = 1;
+			this.titleLabel.Text = "Win-M++";
+			// 
+			// versionLabel
+			// 
+			this.versionLabel.Location = new System.Drawing.Point(50, 47);
+			this.versionLabel.Name = "versionLabel";
+			this.versionLabel.Size = new System.Drawing.Size(168, 16);
+			this.versionLabel.TabIndex = 2;
+			this.versionLabel.Text = "version will load here";
+			// 
+			// linkLabel
+			// 
+			this.linkLabel.Location = new System.Drawing.Point(12, 63);
+			this.linkLabel.Name = "linkLabel";
+			this.linkLabel.Size = new System.Drawing.Size(205, 18);
+			this.linkLabel.TabIndex = 3;
+			this.linkLabel.TabStop = true;
+			this.linkLabel.Text = "https://github.com/bnfour/winmplusplus3";
+			this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelLinkClicked);
+			// 
+			// labelInfo
+			// 
+			this.labelInfo.Location = new System.Drawing.Point(107, 81);
+			this.labelInfo.Name = "labelInfo";
+			this.labelInfo.Size = new System.Drawing.Size(110, 21);
+			this.labelInfo.TabIndex = 4;
+			this.labelInfo.Text = "by Bn4, January 2018";
+			// 
+			// AboutForm
+			// 
+			this.ClientSize = new System.Drawing.Size(229, 111);
+			this.Controls.Add(this.labelInfo);
+			this.Controls.Add(this.linkLabel);
+			this.Controls.Add(this.versionLabel);
+			this.Controls.Add(this.titleLabel);
+			this.Controls.Add(this.pictureBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutForm";
+			this.ShowIcon = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "About Win-M++";
+			this.Shown += new System.EventHandler(this.AboutFormShown);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ ui/AboutForm.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace winmplusplus3
+{
+	/// <summary>
+	/// Description of AboutForm.
+	/// </summary>
+	public partial class AboutForm : Form
+	{
+		public AboutForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+		
+		/// <summary>
+		/// Form Shown event handler.
+		/// Loads actual version info.
+		/// </summary>
+		/// <param name="sender">Event sender, AboutForm itself.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void AboutFormShown(object sender, EventArgs e)
+		{
+			versionLabel.Text = "version " + Application.ProductVersion;
+		}
+		
+		/// <summary>
+		/// linkLabel Click event handler.
+		/// Opens project's github repo from link.
+		/// </summary>
+		/// <param name="sender">Event sender, linkLabel.</param>
+		/// <param name="e">Event arguments, unused.</param>
+		private void LinkLabelLinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start(linkLabel.Text);
+		}
+	}
+}
