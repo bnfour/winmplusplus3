b538a2f5fe2c4c8718b03da17ff6bc3c7e56ff72
Bn4 <bananbl4@gmail.com>
2018-01-22 11:08:04 +0700

    IFilter interface added

---
+++ logic/IFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace winmplusplus3
+{
+	/// <summary>
+	/// An interface for Window filter.
+	/// Takes List<Window> as argument, filters off some Windows based on some criteria and returns filtered list.
+	/// </summary>
+	public interface IFilter
+	{
+		/// <summary>
+		/// The filter method.
+		/// </summary>
+		/// <param name="toFilter">List of Windows to filter.</param>
+		/// <returns>Filtered list.</returns>
+		List<Window> Filter(List<Window> toFilter);
+	}
+}
