4d2848b184a607809bcf603d5f0534516a411de2
Bn4 <bananbl4@gmail.com>
2018-01-22 13:53:27 +0700

    Fix the AboutForm design a little

--- ui/AboutForm.Designer.cs
+++ ui/AboutForm.Designer.cs
@@ -46,50 +46,57 @@ namespace winmplusplus3
 			// 
 			this.pictureBox.Image = global::winmplusplus3.Resources.about_logo;
 			this.pictureBox.InitialImage = null;
-			this.pictureBox.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox.Location = new System.Drawing.Point(14, 12);
 			this.pictureBox.Name = "pictureBox";
-			this.pictureBox.Size = new System.Drawing.Size(32, 32);
+			this.pictureBox.Size = new System.Drawing.Size(65, 48);
+			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pictureBox.TabIndex = 0;
 			this.pictureBox.TabStop = false;
 			// 
 			// titleLabel
 			// 
-			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.titleLabel.Location = new System.Drawing.Point(50, 12);
+			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.titleLabel.Location = new System.Drawing.Point(85, 12);
 			this.titleLabel.Name = "titleLabel";
-			this.titleLabel.Size = new System.Drawing.Size(168, 32);
+			this.titleLabel.Size = new System.Drawing.Size(133, 32);
 			this.titleLabel.TabIndex = 1;
 			this.titleLabel.Text = "Win-M++";
+			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// versionLabel
 			// 
-			this.versionLabel.Location = new System.Drawing.Point(50, 47);
+			this.versionLabel.Location = new System.Drawing.Point(85, 47);
 			this.versionLabel.Name = "versionLabel";
-			this.versionLabel.Size = new System.Drawing.Size(168, 16);
+			this.versionLabel.Size = new System.Drawing.Size(133, 16);
 			this.versionLabel.TabIndex = 2;
 			this.versionLabel.Text = "version will load here";
+			this.versionLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			// 
 			// linkLabel
 			// 
-			this.linkLabel.Location = new System.Drawing.Point(12, 63);
+			this.linkLabel.AutoSize = true;
+			this.linkLabel.Location = new System.Drawing.Point(14, 63);
 			this.linkLabel.Name = "linkLabel";
-			this.linkLabel.Size = new System.Drawing.Size(205, 18);
+			this.linkLabel.Size = new System.Drawing.Size(203, 13);
 			this.linkLabel.TabIndex = 3;
 			this.linkLabel.TabStop = true;
 			this.linkLabel.Text = "https://github.com/bnfour/winmplusplus3";
+			this.linkLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelLinkClicked);
 			// 
 			// labelInfo
 			// 
-			this.labelInfo.Location = new System.Drawing.Point(107, 81);
+			this.labelInfo.AutoSize = true;
+			this.labelInfo.Location = new System.Drawing.Point(108, 76);
 			this.labelInfo.Name = "labelInfo";
-			this.labelInfo.Size = new System.Drawing.Size(110, 21);
+			this.labelInfo.Size = new System.Drawing.Size(110, 13);
 			this.labelInfo.TabIndex = 4;
 			this.labelInfo.Text = "by Bn4, January 2018";
+			this.labelInfo.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// AboutForm
 			// 
-			this.ClientSize = new System.Drawing.Size(229, 111);
+			this.ClientSize = new System.Drawing.Size(229, 94);
 			this.Controls.Add(this.labelInfo);
 			this.Controls.Add(this.linkLabel);
 			this.Controls.Add(this.versionLabel);
@@ -106,6 +113,7 @@ namespace winmplusplus3
 			this.Shown += new System.EventHandler(this.AboutFormShown);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
